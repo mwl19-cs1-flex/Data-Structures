cf069e2e524701e7f509b69df0b34b8d7ff35e7c
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-03-26 16:15:55 -0700

    Completed in order print

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -126,11 +126,10 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        print(node)
-        
-        if self.left:
+        if self.left is not None:
             self.left.in_order_print(self.left)
-        if self.right:
+        print(node.value)
+        if self.right is not None:
             self.right.in_order_print(self.right)
 
     # Print the value of every node, starting with the given node,
